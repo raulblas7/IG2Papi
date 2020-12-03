48bf0f857e706c1153f6c5b5c90750220f524848
Anass <anasca01@ucm.es>
2020-12-04 00:54:56 +0100

    rula

--- IG2/ProyectosOGREvc15x86/media/materials/scripts/HolesFS.glsl
+++ IG2/ProyectosOGREvc15x86/media/materials/scripts/HolesFS.glsl
@@ -1,38 +1,45 @@
 #version 330 core
 
-uniform sampler2D textura1; // tipo sampler2D para texturas 2D
 
 in vec2 vUv0; // out del vertex shader
+in vec3 Normal;
 
-uniform vec4 InColor;
-uniform vec4 OutColor; 
 
-uniform float Flipping;
-out vec4 fFragColor; // out del fragment shader
+uniform sampler2D textura1; // tipo sampler2D para texturas 2D
+
+uniform vec3 color_front;   // color del que se pintara la parte de fuera
+uniform vec3 color_back;    // color del que se pintara la parte de dentro
+
 
 uniform vec4 lightPos;      // direccion de la luz (considerada como vector)
 uniform vec4 intLuz;        // intensidad de luz
 
+out vec4 fFragColor; // out del fragment shader
+
 void main() {
-vec3 color = vec3(texture(texture1, vUv0));
-
-    if(color.x > 0.55) discard;         // si el texel cumple esta caracteristica, salimos y no se pinta (como un return)
-
-    vec3 norm;
-	//bool frontFacing = (Flipping > -1)? gl_FrontFacing : ! gl_FrontFacing;
-    if (gl_FrontFacing)
-	{
-		norm = normalize(Normal); 
-		 color = InColor;
-    }
-	else
-	{
-		norm = -normalize(Normal);
-		  color = OutColor;
-	}
-
-        vec3 lightDir = normalize(vec3(lightPos));
+	vec3 color = vec3(texture(textura1, vUv0));
+	 if(color.x > 0.5) discard;         // si el texel cumple esta caracteristica, salimos y no se pinta (como un return)
+ 
+ 
+ if(gl_FrontFacing){ 
+	vec3 norm = -normalize(Normal);
+	 vec3 lightDir = normalize(vec3(lightPos));
         float diff = max(dot(norm, lightDir), 0.0);
         vec3 diffuse = vec3(intLuz) * diff;
-        fFragColor = vec4(color * diffuse, 1.0);
+    fFragColor = vec4(color_front*color *diffuse , 1.0);
+ }
+ else
+  {
+	vec3  norm = normalize(Normal);
+	  vec3 lightDir = normalize(vec3(lightPos));
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = vec3(intLuz) * diff;
+      // fFragColor = vec4(color_front*color , 1.0);
+	    fFragColor = vec4(color_back*color , 1.0);
+
+  }
+
+       
+
+
 }
--- IG2/ProyectosOGREvc15x86/media/materials/scripts/SpaceFS.glsl
+++ IG2/ProyectosOGREvc15x86/media/materials/scripts/SpaceFS.glsl
@@ -4,23 +4,15 @@ uniform sampler2D textura1; // tipo sampler2D para texturas 2D
 
 uniform sampler2D textura2; // ‐> unidades de textura (int)
 
-uniform float BF; // blending factor
-
-uniform float intLuzAmb; // luz ambiente blanca
-
 in vec2 vUv0; // out del vertex shader
 in vec2 vUv1; // out del vertex shader
 
 out vec4 fFragColor; // out del fragment shader
 
 void main() {
-	/*vec3 color1 = vec3(texture(textura1, vUv0)); // acceso a téxel
-	vec3 color2 = vec3(texture(textura2, vUv0)); // configuración!
-	vec3 color = mix(color1, color2, BF) * intLuzAmb;
-	// mix ‐> (1‐BF).color1 + BF.color2*/
 
-    vec3 color1 = vec3(texture(textura1, vUv1)); 
-	vec3 color2 = vec3(texture(textura2, vUv0));
+    vec3 color1 = vec3(texture(textura1, vUv0)); 
+	vec3 color2 = vec3(texture(textura2, vUv1));
 	vec3 color = color1*color2;         
 
 
