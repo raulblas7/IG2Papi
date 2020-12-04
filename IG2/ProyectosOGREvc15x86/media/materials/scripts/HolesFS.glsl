716de534c184d79ef24b65b3ceed50ed0148b3e7
Anass <anasca01@ucm.es>
2020-12-04 01:30:44 +0100

    Terminado creo

--- IG2/ProyectosOGREvc15x86/media/materials/scripts/HolesFS.glsl
+++ IG2/ProyectosOGREvc15x86/media/materials/scripts/HolesFS.glsl
@@ -2,10 +2,13 @@
 
 
 in vec2 vUv0; // out del vertex shader
+in vec2 vUv1; // out del vertex shader
+
 in vec3 Normal;
 
 
 uniform sampler2D textura1; // tipo sampler2D para texturas 2D
+uniform sampler2D textura2; // tipo sampler2D para texturas 2D
 
 uniform vec3 color_front;   // color del que se pintara la parte de fuera
 uniform vec3 color_back;    // color del que se pintara la parte de dentro
@@ -18,6 +21,8 @@ out vec4 fFragColor; // out del fragment shader
 
 void main() {
 	vec3 color = vec3(texture(textura1, vUv0));
+	vec3 color2 = vec3(texture(textura2, vUv1));
+
 	 if(color.x > 0.5) discard;         // si el texel cumple esta caracteristica, salimos y no se pinta (como un return)
  
  
@@ -26,7 +31,7 @@ void main() {
 	 vec3 lightDir = normalize(vec3(lightPos));
         float diff = max(dot(norm, lightDir), 0.0);
         vec3 diffuse = vec3(intLuz) * diff;
-    fFragColor = vec4(color_front*color *diffuse , 1.0);
+    fFragColor = vec4(color_front*color2*diffuse  , 1.0);
  }
  else
   {
@@ -34,8 +39,7 @@ void main() {
 	  vec3 lightDir = normalize(vec3(lightPos));
         float diff = max(dot(norm, lightDir), 0.0);
         vec3 diffuse = vec3(intLuz) * diff;
-      // fFragColor = vec4(color_front*color , 1.0);
-	    fFragColor = vec4(color_back*color , 1.0);
+	    fFragColor = vec4(color_back*color*diffuse  , 1.0);
 
   }
 
--- IG2/ProyectosOGREvc15x86/media/materials/scripts/HolesVS.glsl
+++ IG2/ProyectosOGREvc15x86/media/materials/scripts/HolesVS.glsl
@@ -4,14 +4,19 @@ in vec4 vertex;                     // atributos de los vertices a procesar
 in vec3 normal;
 in vec2 uv0;
 
+
 uniform mat4 modelViewProjMat;      // matriz de transformacion
 uniform mat4 normalMat;             // matriz inversa (normales)
 
 out vec2 vUv0;                 // out del vertex shader
+out vec2 vUv1;                 // out del vertex shader
+
 out vec3 Normal;
 
 void main() {
 	Normal = mat3(normalMat)*normal;
     vUv0 = uv0;
+    vUv1 = uv0;
+
 	gl_Position = modelViewProjMat * vertex; // obligatorio
 }
